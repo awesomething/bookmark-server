0d1f317209e0509862fd5ce808815774eaea9d0a
Henry Ebomah <henryebomah@gmail.com>
2021-03-05 12:33:15 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const bookmarksRouter = require('./bookmarks/bookmarks-router')
+
 
 
 const app = express()
